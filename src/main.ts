d5a901c314327a7279f7f1051efcb7f3cd6b1366
Ocyss <qiudie@88.com>
2024-02-26 16:28:40 +0800

    init: 🎉

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { ElMessage, ElMessageBox } from "element-plus";
+import "./style.css";
+import "element-plus/dist/index.css";
+
+function start() {
+  console.log("BoosHelper加载成功!");
+  ElMessage("BoosHelper加载成功!");
+}
+
+start();
---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
+import monkey, { cdn } from "vite-plugin-monkey";
+import process from "process";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    monkey({
+      entry: "src/main.ts",
+      userscript: {
+        name: "Boss直聘助手",
+        version: loadEnv("", process.cwd(), "").VITE_VERSION,
+        license: "MIT",
+        description:
+          "优化UI去除广告,批量投递简历,高级筛选,GPT自动打招呼,多账号管理...",
+        icon: "https://img.bosszhipin.com/beijin/mcs/banner/3e9d37e9effaa2b6daf43f3f03f7cb15cfcd208495d565ef66e7dff9f98764da.jpg",
+        namespace: "https://github.com/Ocyss/boos-helper",
+        homepage: "https://github.com/Ocyss/boos-helper",
+        match: ["https://www.zhipin.com/*"],
+        author: "Ocyss",
+        "run-at": "document-start",
+      },
+      build: {
+        externalGlobals: {
+          vue: cdn.jsdelivr("Vue", "dist/vue.global.prod.js"),
+        },
+      },
+    }),
+  ],
+});
